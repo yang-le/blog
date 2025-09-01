cde401368be7d183ffd24636c6d2410151d2a8b4
Yang Le <yangle0125@qq.com>
2025-09-01 22:21:15 +0800

    update

--- docs/javascripts/katex.js
+++ docs/javascripts/katex.js
@@ -1,8 +1,8 @@
-document$.subscribe(({ body }) => { 
+document$.subscribe(({ body }) => {
   renderMathInElement(body, {
     delimiters: [
-      { left: "$$",  right: "$$",  display: true },
-      { left: "$",   right: "$",   display: false },
+      { left: "$$", right: "$$", display: true },
+      { left: "$", right: "$", display: false },
       { left: "\\(", right: "\\)", display: false },
       { left: "\\[", right: "\\]", display: true }
     ],
